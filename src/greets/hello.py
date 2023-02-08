61c380b239d18a0d747082aa70ea40198229f228
Tqkoyaki <np768@scarletmail.rutgers.edu>
2023-02-08 16:36:15 -0500

    Adjusted Packaging

--- src/greets/hello.py
+++
@@ -1,8 +0,0 @@
-class HelloWorld:
-    def hello(txt="World"):
-        """ Prints Hello World to the console
-
-        Args:
-            txt (str, optional): Prints the text with hello. Defaults to "World".
-        """
-        print(f"Hello {txt}")
